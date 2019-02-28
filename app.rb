be8b9519afe416e32700174fea5681e7af21562e
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-28 07:57:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,11 +14,12 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase].gsub! '%20', ' '
+    @phrase = params[:phrase]
     binding.pry
     string = "#{@phrase}"
     (@number-1).times do
-      string + " #{@phrase}"
+      string += " #{@phrase}"
     end
+    string
   end
 end
